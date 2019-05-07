4df7e10cd8add1f7a5d72be64b28069ac4c4c12b
Smara9dus <emeraldbay53@gmail.com>
2019-05-07 17:07:39 -0500

    Cleanup

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,15 @@ public class Main {
 
     public static void main(String[] args) {    // TODO: get posting to FB
 
+        TwitterPoster tp = new TwitterPoster();
+        HeadlessSimple headlessSimple = new HeadlessSimple();
+
         // First get the previous tweet and add its likes into likes.txt
-//        TwitterPoster tp = new TwitterPoster();
-//        try {
-//            System.out.println(tp.getOptions());
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
+        // tp.getLikes();
 
         // Generate image as "recent.png"
-        HeadlessSimple headlessSimple = new HeadlessSimple();
+
         String text = headlessSimple.script(); // This is the text that will be posted along with the image
-        System.out.println(text);
 
         // Post image "recent.png"
 
--- src/TwitterPoster.java
+++ src/TwitterPoster.java
@@ -1,2 +1,19 @@
+import java.io.File;
+
 public class TwitterPoster {
+
+    // Get likes from previous tweet
+    public void getLikes()  {
+        File file = new File("likes.txt");
+    }
+
+    // Post image
+    public void postImage(String text)  {
+        // Post image with filepath "recent.png" along with message 'text'
+    }
+
+
+
+
+
 }
