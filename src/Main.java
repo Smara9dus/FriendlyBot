a09845704d13d59415a2c707e26058bb9fb70524
Zain <zchaudhr@macalester.edu>
2019-05-08 18:45:54 -0500

    Added a TwitterBot that posts images!

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,12 @@ public class Main {
 
         // First get the previous tweet and add its likes into likes.txt
         // tp.getLikes();
+//        tp.configBuilder();
 
         // Generate image as "recent.png"
 
         String text = headlessSimple.script(); // This is the text that will be posted along with the image
+        tp.postImage(text);
 
         // Post image "recent.png"
 
--- src/TwitterPoster.java
+++ src/TwitterPoster.java
@@ -1,6 +1,17 @@
+import twitter4j.*;
+import twitter4j.auth.AccessToken;
+import twitter4j.conf.ConfigurationBuilder;
+
 import java.io.File;
+import java.util.List;
 
 public class TwitterPoster {
+
+    static String consumerKey = "XXXXXX";
+    static String consumerKeySecret = "XXXXX";
+    static String accessToken = "XXXXXXX";
+    static String accessTokenSecret = "XXXXXXXX";
+
 
     // Get likes from previous tweet
     public void getLikes()  {
@@ -10,10 +21,31 @@ public class TwitterPoster {
     // Post image
     public void postImage(String text)  {
         // Post image with filepath "recent.png" along with message 'text'
+        try {
+        Twitter twitter = new TwitterFactory().getInstance();
+        twitter.setOAuthConsumer(consumerKey, consumerKeySecret);
+        AccessToken t = new AccessToken(accessToken, accessTokenSecret);
+
+        twitter.setOAuthAccessToken(t);
+        String statusMessage = text;
+        File file = new File("recent.png");
+
+        StatusUpdate status = new StatusUpdate(statusMessage);
+        status.setMedia(file); // set the image to be uploaded here.
+        twitter.updateStatus(status);
+
+        System.out.println("Successfully updated the status in Twitter.");
+        } catch (TwitterException te) {
+            te.printStackTrace();
+        }
     }
 
 
-
-
-
 }
+
+
+
+
+
+
+
